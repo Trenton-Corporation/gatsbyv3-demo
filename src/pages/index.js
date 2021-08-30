db0674c8ec78693f67bd63fb6fcc3aa0755e364b
Adrian Ross <adriantoddross@gmail.com>
2021-08-30 16:24:31 -0400

    Stub typography

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,11 @@ import React from "react"
 import { Grid, Cell } from "baseui/layout-grid"
 import { Card, StyledBody, StyledAction } from "baseui/card"
 import { Button } from "baseui/button"
+import { H2, H3 } from "baseui/typography"
 
 import Layout from "../components/Layout"
 
 export default function Home() {
-
-
   return (
     <Layout>
       <Grid>
@@ -22,10 +21,13 @@ export default function Home() {
         <Cell span={4}>
           <Card>
             <StyledBody>
-              Proin ut dui sed metus pharetra hend rerit vel non mi. Nulla
-              ornare faucibus ex, non facilisis nisl. Proin ut dui sed metus
-              pharetra hend rerit vel non mi. Nulla ornare faucibus ex, non
-              facilisis nisl.
+              <H2>Welcome</H2>
+              Trenton products provide effective, long-lasting anticorrosion
+              protection for your resources. Our products include a variety of
+              Wax-Tape® brand anticorrosion wraps for applications in
+              aboveground and belowground settings, hot-applied and cold-applied
+              waxes, outerwraps for additional mechanical protection and casing
+              filling for pipeline and cable casings. Learn more.
             </StyledBody>
             <StyledAction>
               <Button
@@ -41,10 +43,11 @@ export default function Home() {
         <Cell span={4}>
           <Card>
             <StyledBody>
-              Proin ut dui sed metus pharetra hend rerit vel non mi. Nulla
-              ornare faucibus ex, non facilisis nisl. Proin ut dui sed metus
-              pharetra hend rerit vel non mi. Nulla ornare faucibus ex, non
-              facilisis nisl.
+              <H2>Quick Links</H2>
+              <H3>How can we help?</H3>
+              How can we help? Wax-Tape® Brand Anticorrosion Wraps Primers &
+              Other Products Innercoat® Hot-Applied Wax Coating System
+              Outerwraps Casing Filling Patch-Pad® Exothermic Weld Protector
             </StyledBody>
             <StyledAction>
               <Button
@@ -60,10 +63,16 @@ export default function Home() {
         <Cell span={4}>
           <Card>
             <StyledBody>
-              Proin ut dui sed metus pharetra hend rerit vel non mi. Nulla
-              ornare faucibus ex, non facilisis nisl. Proin ut dui sed metus
-              pharetra hend rerit vel non mi. Nulla ornare faucibus ex, non
-              facilisis nisl.
+              <H2>Contact Trenton</H2>
+              Corporate Headquarters Ann Arbor, Michigan | (734) 424-3600 North
+              America Midwest USA & Eastern Canada | (734) 368-7528 Southeast
+              USA | (706) 870-2202 Southern USA | (281) 389-2496 Gulf USA |
+              (832) 948-6580 Southwest USA | (432) 553-1543 West Coast USA |
+              (213) 700-3866 Northwest USA & Western Canada | (208) 618-9876
+              International UK, Europe & North Africa | +33 (0) 6 89 58 41 29
+              Middle East & India | +968 92844986 (available via WhatsApp)
+              France & Belgium | +33 (0) 6 73 83 52 24 South America, Asia &
+              Australia | +1 (281) 389-2496 (available via WhatsApp)
             </StyledBody>
             <StyledAction>
               <Button
