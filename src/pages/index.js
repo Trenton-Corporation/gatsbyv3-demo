4688aa7dfeff4d471b37dbfea3be963b17e2ca44
Adrian Ross <adriantoddross@gmail.com>
2021-08-30 15:37:06 -0400

    Stub cards

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,80 @@
 import React from "react"
-import { useStyletron } from "baseui"
 import { Grid, Cell } from "baseui/layout-grid"
+import { Card, StyledBody, StyledAction } from "baseui/card"
+import { Button } from "baseui/button"
 
 import Layout from "../components/Layout"
 
 export default function Home() {
-  const [css, theme] = useStyletron()
-  console.log(theme)
+
+
   return (
     <Layout>
       <Grid>
-        <Cell>
+        <Cell span={12}>
           <img
             src="https://trentoncorp.com/wp-content/themes/trentoncorp/flash/flashhome-placeholder-img.png"
             alt="High quality, long lasting products offering optimal protection from corrosion"
             role="presentation"
             aria-hidden="true"
           />
-          <p
-            className={css({
-              fontSize: "20px",
-              color: theme.colors.primary,
-            })}
-          >
-            Hello world!
-          </p>
+        </Cell>
+        <Cell span={4}>
+          <Card>
+            <StyledBody>
+              Proin ut dui sed metus pharetra hend rerit vel non mi. Nulla
+              ornare faucibus ex, non facilisis nisl. Proin ut dui sed metus
+              pharetra hend rerit vel non mi. Nulla ornare faucibus ex, non
+              facilisis nisl.
+            </StyledBody>
+            <StyledAction>
+              <Button
+                overrides={{
+                  BaseButton: { style: { width: "100%" } },
+                }}
+              >
+                Button Label
+              </Button>
+            </StyledAction>
+          </Card>
+        </Cell>
+        <Cell span={4}>
+          <Card>
+            <StyledBody>
+              Proin ut dui sed metus pharetra hend rerit vel non mi. Nulla
+              ornare faucibus ex, non facilisis nisl. Proin ut dui sed metus
+              pharetra hend rerit vel non mi. Nulla ornare faucibus ex, non
+              facilisis nisl.
+            </StyledBody>
+            <StyledAction>
+              <Button
+                overrides={{
+                  BaseButton: { style: { width: "100%" } },
+                }}
+              >
+                Button Label
+              </Button>
+            </StyledAction>
+          </Card>
+        </Cell>
+        <Cell span={4}>
+          <Card>
+            <StyledBody>
+              Proin ut dui sed metus pharetra hend rerit vel non mi. Nulla
+              ornare faucibus ex, non facilisis nisl. Proin ut dui sed metus
+              pharetra hend rerit vel non mi. Nulla ornare faucibus ex, non
+              facilisis nisl.
+            </StyledBody>
+            <StyledAction>
+              <Button
+                overrides={{
+                  BaseButton: { style: { width: "100%" } },
+                }}
+              >
+                Button Label
+              </Button>
+            </StyledAction>
+          </Card>
         </Cell>
       </Grid>
     </Layout>
