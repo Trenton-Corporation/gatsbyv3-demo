071e156689621cf3173bf44eb1d5be884af1f220
Adrian Ross <adriantoddross@gmail.com>
2021-08-30 15:31:50 -0400

    Add header image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { useStyletron } from "baseui"
+import { Grid, Cell } from "baseui/layout-grid"
 
 import Layout from "../components/Layout"
 
@@ -7,20 +8,25 @@ export default function Home() {
   const [css, theme] = useStyletron()
   console.log(theme)
   return (
-    <Layout
-      className={css({
-        fontSize: "20px",
-        color: theme.colors.primary,
-      })}
-    >
-      <p
-        className={css({
-          fontSize: "20px",
-          color: theme.colors.primary,
-        })}
-      >
-        Hello world!
-      </p>
+    <Layout>
+      <Grid>
+        <Cell>
+          <img
+            src="https://trentoncorp.com/wp-content/themes/trentoncorp/flash/flashhome-placeholder-img.png"
+            alt="High quality, long lasting products offering optimal protection from corrosion"
+            role="presentation"
+            aria-hidden="true"
+          />
+          <p
+            className={css({
+              fontSize: "20px",
+              color: theme.colors.primary,
+            })}
+          >
+            Hello world!
+          </p>
+        </Cell>
+      </Grid>
     </Layout>
   )
 }
