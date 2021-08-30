e48ca8e5dcef8ab8c19f411c94671169a2985bc5
Adrian Ross <adriantoddross@gmail.com>
2021-08-30 12:41:08 -0400

    Test css-in-js styling with BaseUI

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,17 @@
 import React from "react"
+import { useStyletron } from "baseui"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  const [css, theme] = useStyletron()
+  console.log(theme);
+  return (
+    <div
+      className={css({
+        fontSize: "20px",
+        color: theme.colors.primary,
+      })}
+    >
+      Hello world!
+    </div>
+  )
 }
