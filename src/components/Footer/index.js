a55c1bad7c9b6405a4cef1bb5041c279c230eee8
Adrian Ross <adriantoddross@gmail.com>
2021-08-31 14:10:03 -0400

    Add alt attribute

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -23,12 +23,18 @@ export default function NavBar() {
       </List>
       <List $align={ALIGN.center}>
         <Item>
-          All content © 2021 Trenton Corporation. All rights reserved.
-          Call <Link href="tel:+17344243600">(734) 424-3600</Link>
+          All content © 2021 Trenton Corporation. All rights reserved. Call{" "}
+          <Link href="tel:+17344243600">(734) 424-3600</Link>
         </Item>
       </List>
       <List $align={ALIGN.right}>
-        <Item>ISO Certified <img src="https://trentoncorp.com/wp-content/uploads/2016/01/iso-logo.gif"/> </Item>
+        <Item>
+          ISO Certified{" "}
+          <img
+            src="https://trentoncorp.com/wp-content/uploads/2016/01/iso-logo.gif"
+            alt=""
+          />{" "}
+        </Item>
       </List>
     </HeaderNavigation>
   )
